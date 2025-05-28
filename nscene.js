dbad452c2b05e518fadfd8031ee316749999521d
martin <m@marelbsi.de>
2025-05-28 23:07:21 +0200

    first version with thumbstick and trigger support for WebXR and Meta Quest 3S (only Wolvic and Chrome(Meta Link)

--- nscene.js
+++ nscene.js
@@ -122,7 +122,57 @@ let controller1, controller2;
 let controllerGrip1, controllerGrip2;
 let animationLoopStarted = false
 let session;
+let raycaster, intersected = [];
+let tempMatrix = new THREE.Matrix4();
+let group;
+let dolly;
+let cameraVector = new THREE.Vector3(); // create once and reuse it!
+// a variable to store the values from the last polling of the gamepads
+const prevGamePads = new Map();
 
+//default values for speed movement of each axis
+let speedFactor = [10,0, 10.0, 10.0, 10.0];
+let speedMax = 100
+
+
+function onSelectStart(event) {
+	var controller = event.target;
+
+	var intersections = getIntersections(controller);
+
+	if (intersections.length > 0) {
+		var intersection = intersections[0];
+		var object = intersection.object;
+		object.material.emissive.b = 1;
+		controller.attach(object);
+		controller.userData.selected = object;
+	}
+}
+
+function onSelectEnd(event) {
+	var controller = event.target;
+	if (controller.userData.selected !== undefined) {
+		var object = controller.userData.selected;
+		object.material.emissive.b = 0;
+		group.attach(object);
+		controller.userData.selected = undefined;
+	}
+}
+
+function getIntersections(controller) {
+	tempMatrix.identity().extractRotation(controller.matrixWorld);
+	raycaster.ray.origin.setFromMatrixPosition(controller.matrixWorld);
+	raycaster.ray.direction.set(0, 0, -1).applyMatrix4(tempMatrix);
+	return raycaster.intersectObjects(group.children);
+}
+
+function isIterable(obj) {
+	// checks for null and undefined
+	if (obj == null) {
+		return false;
+	}
+	return typeof obj[Symbol.iterator] === "function";
+}
 
 
 function initControlsXR() {
@@ -150,6 +200,12 @@ function initControlsXR() {
    controller2 = renderer.xr.getController( 1 );
    scene.add( controller2 );
 
+   controller1.addEventListener("selectstart", onSelectStart);
+   controller1.addEventListener("selectend", onSelectEnd);
+   controller2.addEventListener("selectstart", onSelectStart);
+   controller2.addEventListener("selectend", onSelectEnd);
+
+
    const controllerModelFactory = new XRControllerModelFactory();
 //   const handModelFactory = new XRHandModelFactory();
 
@@ -170,8 +226,198 @@ function initControlsXR() {
    // hand2 = renderer.xr.getHand( 1 );
    // hand2.add( handModelFactory.createHandModel( hand2 ) );
    // scene.add( hand2 );
+   
+   //Raycaster Geometry
+	var geometry = new THREE.BufferGeometry().setFromPoints([
+		new THREE.Vector3(0, 0, 0),
+		new THREE.Vector3(0, 0, -1)
+	]);
+
+	var line = new THREE.Line(geometry);
+	line.name = "line";
+	line.scale.z = 5;
+
+	controller1.add(line.clone());
+	controller2.add(line.clone());
+
+	raycaster = new THREE.Raycaster();
+
+	group = new THREE.Group();
+	scene.add(group);
+
+	//dolly for camera
+	dolly = new THREE.Group();
+	dolly.position.set(0, 0, 0);
+	dolly.name = "dolly";
+	scene.add(dolly);
+	dolly.add(camera);
+	//add the controls to the dolly also or they will not move with the dolly
+	dolly.add(controller1);
+	dolly.add(controller2);
+	dolly.add(controllerGrip1);
+	dolly.add(controllerGrip2);
+	
 }
 
+
+function dollyMove() {
+	var handedness = "unknown";
+
+	//determine if we are in an xr session
+	const session = renderer.xr.getSession();
+	let i = 0;
+
+	if (session) {
+		let xrCamera = renderer.xr.getCamera(camera);
+		xrCamera.getWorldDirection(cameraVector);
+
+		//a check to prevent console errors if only one input source
+		if (isIterable(session.inputSources)) {
+			for (const source of session.inputSources) {
+				if (source && source.handedness) {
+					handedness = source.handedness; //left or right controllers
+				}
+				if (!source.gamepad) continue;
+				const controller = renderer.xr.getController(i++);
+				const old = prevGamePads.get(source);
+				const data = {
+					handedness: handedness,
+					buttons: source.gamepad.buttons.map((b) => b.value),
+					axes: source.gamepad.axes.slice(0)
+				};
+				if (old) {
+					data.buttons.forEach((value, i) => {
+						//handlers for buttons
+						if (value !== old.buttons[i] || Math.abs(value) > 0.8) {
+							//check if it is 'all the way pushed'
+							if (value === 1) {
+								//console.log("Button" + i + "Down");
+								if (data.handedness == "left") {
+									//console.log("Left Paddle Down");
+									if (i == 1) {
+										dolly.rotateY(-THREE.Math.degToRad(1));
+									}
+									if (i == 3) {
+										//reset teleport to home position
+										dolly.position.x = 0;
+										dolly.position.y = 5;
+										dolly.position.z = 0;
+									}
+								} else {
+									//console.log("Right Paddle Down");
+									if (i == 1) {
+										dolly.rotateY(THREE.Math.degToRad(1));
+									}
+								}
+							} else {
+								// console.log("Button" + i + "Up");
+
+								if (i == 1) {
+									//use the paddle buttons to rotate
+									if (data.handedness == "left") {
+										//console.log("Left Paddle Down");
+										dolly.rotateY(-THREE.Math.degToRad(Math.abs(value)));
+									} else {
+										//console.log("Right Paddle Down");
+										dolly.rotateY(THREE.Math.degToRad(Math.abs(value)));
+									}
+								}
+							}
+						}
+					});
+					data.axes.forEach((value, i) => {
+						//handlers for thumbsticks
+						//if thumbstick axis has moved beyond the minimum threshold from center, windows mixed reality seems to wander up to about .17 with no input
+						if (Math.abs(value) > 0.2) {
+							//set the speedFactor per axis, with acceleration when holding above threshold, up to a max speed
+							speedFactor[i] > speedMax ? (speedFactor[i] = speedMax) : (speedFactor[i] *= 1.10);
+							console.log(value, speedFactor[i], i);
+							if (i == 2) {
+								//left and right axis on thumbsticks
+								if (data.handedness == "left") {
+									// (data.axes[2] > 0) ? console.log('left on left thumbstick') : console.log('right on left thumbstick')
+
+									//move our dolly
+									//we reverse the vectors 90degrees so we can do straffing side to side movement
+									dolly.position.x -= cameraVector.z * speedFactor[i] * data.axes[2];
+									dolly.position.z += cameraVector.x * speedFactor[i] * data.axes[2];
+
+									//provide haptic feedback if available in browser
+									if (
+										source.gamepad.hapticActuators &&
+										source.gamepad.hapticActuators[0]
+									) {
+										var pulseStrength = Math.abs(data.axes[2]) + Math.abs(data.axes[3]);
+										if (pulseStrength > 0.75) {
+											pulseStrength = 0.75;
+										}
+
+										var didPulse = source.gamepad.hapticActuators[0].pulse(
+											pulseStrength,
+											100
+										);
+									}
+								} else {
+									// (data.axes[2] > 0) ? console.log('left on right thumbstick') : console.log('right on right thumbstick')
+									dolly.rotateY(-THREE.Math.degToRad(data.axes[2]));
+								}
+								controlsXR.update();
+							}
+
+							if (i == 3) {
+								//up and down axis on thumbsticks
+								if (data.handedness == "left") {
+									// (data.axes[3] > 0) ? console.log('up on left thumbstick') : console.log('down on left thumbstick')
+									dolly.position.y -= speedFactor[i] * data.axes[3];
+									//provide haptic feedback if available in browser
+									if (
+										source.gamepad.hapticActuators &&
+										source.gamepad.hapticActuators[0]
+									) {
+										var pulseStrength = Math.abs(data.axes[3]);
+										if (pulseStrength > 0.75) {
+											pulseStrength = 0.75;
+										}
+										var didPulse = source.gamepad.hapticActuators[0].pulse(
+											pulseStrength,
+											100
+										);
+									}
+								} else {
+									// (data.axes[3] > 0) ? console.log('up on right thumbstick') : console.log('down on right thumbstick')
+									dolly.position.x -= cameraVector.x * speedFactor[i] * data.axes[3];
+									dolly.position.z -= cameraVector.z * speedFactor[i] * data.axes[3];
+
+									//provide haptic feedback if available in browser
+									if (
+										source.gamepad.hapticActuators &&
+										source.gamepad.hapticActuators[0]
+									) {
+										var pulseStrength = Math.abs(data.axes[2]) + Math.abs(data.axes[3]);
+										if (pulseStrength > 0.75) {
+											pulseStrength = 0.75;
+										}
+										var didPulse = source.gamepad.hapticActuators[0].pulse(
+											pulseStrength,
+											100
+										);
+									}
+								}
+								controlsXR.update();
+							}
+						} else {
+							//axis below threshold - reset the speedFactor if it is greater than zero  or 0.025 but below our threshold
+							if (Math.abs(value) > 0.025) {
+								speedFactor[i] = 0.025;
+							}
+						}
+					});
+				}
+				prevGamePads.set(source, data);
+			}
+		}
+	}
+}
 
 
 // main entry point
@@ -1295,7 +1541,7 @@ function checkXRController(delta){
 		move.addScaledVector(forward, -yAxis * speed * delta);
 		move.addScaledVector(right, xAxis * speed * delta);
 
-		controlsXR.getObject().position.add(move);
+		controls.getObject().position.add(move);
 		// alert("xAxis = "+xAxis+" yAxis = "+aAxis);
 
 	}
@@ -1562,6 +1808,7 @@ function render() {
     // checkIntersect();
     //composer.render(scene, camera);
 
+	dollyMove();
     renderer.render( scene, camera );
 
 }
